e215a318e5266823b6ecfe51fcd1dc062c0dd1a4
Valuimnee <tsalapovadisco@gmail.com>
2018-02-28 22:13:45 +0300

    Solved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  var primes=[];
+  var degrees=[];
+  var numberDegrees=[];
+  var i=2;
+  while(base>1){
+      if(base%i==0){
+        primes.push(i);
+        var degree=1;
+        base=base/i | 0;
+        while(base%i==0){
+          base=base/i | 0;
+          degree++;
+        }
+        degrees.push(degree);
+      }
+      i++;
+  }
+  console.log(primes);
+  console.log(degrees);
+  var currNumber;
+  var degree;
+  for(var j=0; j<primes.length; j++){
+    currNumber=number;
+    degree=0;
+    while(currNumber/primes[j] | 0 >0){
+      currNumber= currNumber/primes[j] | 0;
+      degree+=currNumber;
+    }
+    numberDegrees.push(degree/degrees[j] | 0);
+  }
+  console.log(numberDegrees);
+  return Math.min(...numberDegrees);
 }
